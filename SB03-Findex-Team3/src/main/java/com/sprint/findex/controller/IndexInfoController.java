222135d47a861351d87e8f98205cfb1edd39eb23
wangcoJiin <mf0d070208@gmail.com>
2025-06-12 14:29:09 +0900

    refactor: api 인터페이스와 중복되는 예외 반환 로직 삭제

--- SB03-Findex-Team3/src/main/java/com/sprint/findex/controller/IndexInfoController.java
+++ SB03-Findex-Team3/src/main/java/com/sprint/findex/controller/IndexInfoController.java
@@ -4,7 +4,6 @@ import com.sprint.findex.controller.api.IndexInfoApi;
 import com.sprint.findex.dto.request.IndexInfoCreateCommand;
 import com.sprint.findex.dto.request.IndexInfoCreateRequest;
 import com.sprint.findex.dto.request.IndexInfoUpdateRequest;
-import com.sprint.findex.dto.response.ErrorResponse;
 import com.sprint.findex.dto.response.IndexInfoDto;
 import com.sprint.findex.dto.response.IndexInfoSearchDto;
 import com.sprint.findex.dto.response.IndexInfoSummaryDto;
@@ -13,7 +12,6 @@ import com.sprint.findex.mapper.IndexInfoSearchMapper;
 import com.sprint.findex.service.IndexInfoService;
 import jakarta.validation.Valid;
 import java.util.List;
-import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -40,24 +38,11 @@ public class IndexInfoController implements IndexInfoApi {
     @Override
     @GetMapping("/{id}")
     public ResponseEntity<Object> getIndexInfo(@PathVariable("id") Long id) {
-        try {
-            IndexInfoDto indexInfo = indexInfoService.findById(id);
-            return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(indexInfo);
-        }
-        catch (NoSuchElementException e) {
-            return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponse(404, "조회할 지수 정보를 찾을 수 없음", "ID가 " + id + "인 지수 정보가 존재하지 않습니다.")
-            );
-        }
-        catch (Exception e) {
-            return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse(500, "서버 오류")
-            );
-        }
+        IndexInfoDto indexInfo = indexInfoService.findById(id);
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(indexInfo);
     }
 
     @Override
@@ -72,59 +57,38 @@ public class IndexInfoController implements IndexInfoApi {
         @RequestParam(defaultValue = "asc", required = false) String sortDirection,
         @RequestParam(defaultValue = "20", required = false) int size
     ) {
-        try {
-            IndexInfoSearchDto searchDto = indexInfoSearchMapper.toDto(
-                indexClassification,
-                indexName,
-                favorite,
-                idAfter,
-                cursor,
-                sortField,
-                sortDirection,
-                size
-            );
+        IndexInfoSearchDto searchDto = indexInfoSearchMapper.toDto(
+            indexClassification,
+            indexName,
+            favorite,
+            idAfter,
+            cursor,
+            sortField,
+            sortDirection,
+            size
+        );
 
-            CursorPageResponseIndexInfoDto response = indexInfoService.findIndexInfoByCursor(searchDto);
-            return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(response);
+        CursorPageResponseIndexInfoDto response = indexInfoService.findIndexInfoByCursor(searchDto);
 
-        }
-        catch (IllegalArgumentException e) {
-            return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponse(400, "잘못된 요청 (유효하지 않은 필터 값 등)")
-            );
-        }
-        catch (Exception e) {
-            return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse(500, "서버 오류")
-            );
-        }
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(response);
     }
 
     @GetMapping("/summaries")
     public ResponseEntity<Object> getIndexInfoSummaries() {
-        try {
-            List<IndexInfoSummaryDto> indexInfoSummary = indexInfoService.findIndexInfoSummary();
-            return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(indexInfoSummary);
-        }
-        catch (Exception e) {
-            return ResponseEntity
-                .status(HttpStatus
-                    .INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse(500, "서버 오류")
-            );
-        }
+        List<IndexInfoSummaryDto> indexInfoSummary = indexInfoService.findIndexInfoSummary();
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(indexInfoSummary);
     }
 
     @PostMapping
     public ResponseEntity<IndexInfoDto> createIndexInfo(@Valid @RequestBody IndexInfoCreateRequest request) {
         IndexInfoCreateCommand command = IndexInfoCreateCommand.fromUser(request);
         IndexInfoDto indexInfoDto = indexInfoService.createIndexInfo(command);
+
         return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(indexInfoDto);
@@ -134,6 +98,7 @@ public class IndexInfoController implements IndexInfoApi {
     public ResponseEntity<IndexInfoDto> updateIndexInfo(@PathVariable Long id,
         @Valid @RequestBody IndexInfoUpdateRequest request) {
         IndexInfoDto updatedIndex = indexInfoService.updateIndexInfo(id, request);
+
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(updatedIndex);
@@ -142,6 +107,7 @@ public class IndexInfoController implements IndexInfoApi {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteIndexInfo(@PathVariable Long id) {
         indexInfoService.deleteIndexInfo(id);
+
         return ResponseEntity
             .noContent()
             .build();

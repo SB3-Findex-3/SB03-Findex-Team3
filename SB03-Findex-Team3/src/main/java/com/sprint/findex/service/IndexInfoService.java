b2cf1baf38f04ac24947d2bd4136cbccdf6493e2
LZHTK <gusdnjsrl2@gmail.com>
2025-06-12 11:41:54 +0900

    remove: 미사용 파일 및 메서드 삭제

--- SB03-Findex-Team3/src/main/java/com/sprint/findex/service/IndexInfoService.java
+++ SB03-Findex-Team3/src/main/java/com/sprint/findex/service/IndexInfoService.java
@@ -1,11 +1,10 @@
 package com.sprint.findex.service;
 
+import com.sprint.findex.dto.IndexInfoSearchDto;
 import com.sprint.findex.dto.request.IndexInfoCreateCommand;
 import com.sprint.findex.dto.request.IndexInfoUpdateRequest;
-import com.sprint.findex.dto.response.IndexInfoDto;
-import com.sprint.findex.entity.IndexInfo;
-import com.sprint.findex.dto.IndexInfoSearchDto;
 import com.sprint.findex.dto.response.CursorPageResponseIndexInfoDto;
+import com.sprint.findex.dto.response.IndexInfoDto;
 import com.sprint.findex.dto.response.IndexInfoSummaryDto;
 import java.util.List;
 
@@ -14,9 +13,6 @@ public interface IndexInfoService {
 
     // Index Info 생성
     IndexInfoDto createIndexInfo(IndexInfoCreateCommand command);
-
-    // Open_API로 Index Info 생성
-    IndexInfo createIndexInfoFromApi(IndexInfoCreateCommand command);
 
     // Index Info 업데이트
     IndexInfoDto updateIndexInfo(Long id, IndexInfoUpdateRequest updateDto);

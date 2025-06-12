ba6c87699439e4597f91e2f7a9c5075b2e74a51e
wangcoJiin <mf0d070208@gmail.com>
2025-06-12 09:24:42 +0900

    refactor: 안 쓰는 주석 삭제

--- SB03-Findex-Team3/src/main/java/com/sprint/findex/service/IndexInfoService.java
+++ SB03-Findex-Team3/src/main/java/com/sprint/findex/service/IndexInfoService.java
@@ -12,24 +12,17 @@ import java.util.List;
 
 public interface IndexInfoService {
 
-    // Index Info 생성
     IndexInfoDto createIndexInfo(IndexInfoCreateCommand command);
 
-    // Open_API로 Index Info 생성
     IndexInfo createIndexInfoFromApi(IndexInfoCreateCommand command);
 
-    // Index Info 업데이트
     IndexInfoDto updateIndexInfo(Long id, IndexInfoUpdateRequest updateDto);
 
-    // Index Info 삭제
     void deleteIndexInfo(Long id);
 
-    // Index Info 조회
     IndexInfoDto findById(Long id);
 
-    // Index Info 목록 조회
     CursorPageResponseIndexInfoDto findIndexInfoByCursor(IndexInfoSearchDto searchDto);
 
-    // Index Info 요약 목록 조회
     List<IndexInfoSummaryDto> findIndexInfoSummary();
 }
